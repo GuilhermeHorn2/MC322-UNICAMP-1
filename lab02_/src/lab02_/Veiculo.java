26cf90bd82ba165dccd0a07053507b3b0b1bf2c6
Guilherme Azevedo Horn <g247127@dac.unicamp.br7>
2023-03-15 21:47:40 -0300

    lab02 aqui!

--- lab02_/src/lab02_/Veiculo.java
+++ lab02_/src/lab02_/Veiculo.java
@@ -1,5 +1,38 @@
 package lab02_;
 
 public class Veiculo {
+	private String _placa;
+	private String _marca;
+	private String _modelo;
+	
+	//contrutores:
+	public Veiculo() {}
+	public Veiculo(String placa,String marca,String modelo) {
+		_placa = placa;
+		_marca = marca;
+		_modelo = modelo;
+	}
+	
+	//gets
+	public String getPlaca(){
+		return _placa;
+	}
+	public String getMarca(){
+		return _marca;
+	}	
+	public String getModelo(){
+		return _modelo;
+	}	
+	
+	//sets
+	public void setPlaca(String placa) {
+		_placa = placa;
+	}
+	public void setMarca(String marca) {
+		_placa = marca;
+	}
+	public void setModelo(String modelo) {
+		_placa = modelo;
+	}
 
 }
