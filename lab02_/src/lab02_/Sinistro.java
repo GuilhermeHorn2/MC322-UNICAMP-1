45ccbfac817c0f4d2dfccce57b1017d43eafd8aa
Guilherme Azevedo Horn <g247127@dac.unicamp.br7>
2023-03-23 21:20:33 -0300

    lab02 aqui!

--- lab02_/src/lab02_/Sinistro.java
+++ lab02_/src/lab02_/Sinistro.java
@@ -7,9 +7,11 @@ public class Sinistro {
       private String _data;
       private String _endereco;
       //é preciso de um contador para colocar os ids no vetor_id
-      private int contador = 0;
+      
+      private static int contador = 0;
       //vou assumir 999 como o numero maximo de objetos e ids que podem ser gerados
-      private int[] vetor_id = new int[999];
+      
+      private static int[] vetor_id = new int[999];
       
 
       //construtor
@@ -17,11 +19,13 @@ public class Sinistro {
     	  _data = data;
     	  _endereco = endereco;
     	  //para criar um objeto: Sinistro s = new Sinistro(data,endereco)
+    	  
     	  _id = gerar_id();
     	  
     	  /*Como é preciso gerar um id único e aleatório,é preciso ter informações de quais ids
     	   * estão indexados em algum objeto,para assim gerar algum id válido
     	   */
+    	  
     	  vetor_id[contador] = _id;
     	 contador += 1;
     	 
@@ -50,12 +54,14 @@ public class Sinistro {
     	  _endereco = endereco;
       }
       public void setid(int id){
+    	  
     	  //não sei se precisa desse,pois o id é gerado automaticamente de forma aleatória
     	  _id = id;
       }
       
       //
       public int na_lista(int i,int[] v) {
+    	  
     	  //retorna 1 se i estiver na lista,0 caso contrario
     	  for(int j = 0;j < v.length;j++) {
     		  if(i == v[j]) {
@@ -66,11 +72,13 @@ public class Sinistro {
       }
       
       public int gerar_id(){
+    	  
     	  //aqui eu procuro um id aleatório até chegar em algum que não esteja em vetor_id
     	  int id = -1;
     	  
     	  while(true) {
     		  if(vetor_id[998] != 0) {
+    			  
     			  //proteção pra não cair em um loop infinito caso os ids estejam estejam preenchidos de 1-999
     			  System.out.println("Numero maximo de objetos instanciados atingido");
     			  break;
