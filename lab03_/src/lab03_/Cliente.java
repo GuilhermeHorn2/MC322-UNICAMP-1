f7a6e59d068558a8c493cf07549dd02809bd5b0b
Guilherme Azevedo Horn <g247127@dac.unicamp.br7>
2023-04-03 09:51:09 -0300

    ainda em teste

--- lab03_/src/lab03_/Cliente.java
+++ lab03_/src/lab03_/Cliente.java
@@ -1,5 +1,32 @@
 package lab03_;
+import java.util.ArrayList;
 
 public class Cliente {
-
+   private String _nome;
+   private String _endereco;
+   private Date _dataLicensa;
+   private String _educacao;
+   private String _genero;
+   private String _classeEconomica;
+   private static ArrayList<Veiculo> listaVeiculos = new ArrayList<>();
+   
+   /*construtor*/
+   
+   /*a lista de veiculos não começará com nenhum valor,mas atravez do set ela pode ser modificada,pode ser consultado no get*/
+   public Cliente(String nome,String endereco,Date dataLicensa,String educacao,String genero,String classeEconomica) {
+	   _nome = nome;
+	   _endereco = endereco;
+	   _dataLicensa = dataLicensa;
+	   _educacao = educacao;
+	   _genero = genero;
+	   _classeEconomica = classeEconomica;
+   }
+   
+   /*gets*/
+   
+   
+   /*sets*/
+   
+   
+   /*toString*/
 }
